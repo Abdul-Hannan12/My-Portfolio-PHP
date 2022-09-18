d140ed88e764ba41603330f7be9330dbc41be77f
Abdul Hannan <abdulhannan8540680@gmail.com>
2022-09-18 21:35:18 +0500

    initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Portfolio</title>
+
+    <link rel="stylesheet" href="styles/index.css">
+
+    <script src="https://kit.fontawesome.com/0980d95b57.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <?php include("header.html") ?>
+    
+    <div class="cover">
+        
+        <div class="heading">
+        <h3 class="name_heading">Abdul Hannan</h3>
+        <p class="name_sub_heading">WEB AND APP DEVELOPER</p>
+        </div>
+
+        <div class="heading_para">
+        <p class="head_para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ex eum quia, fugiat delectus error adipisci vitae ab ducimus excepturi eligendi ad aliquid recusandae?</p>
+        <div class="icons">
+            <a target="_blank" href="https://github.com/Rocky-Khan"> <i style="color: white;" class="fa-brands fa-github"></i> </a>
+            <a target="_blank" href="https://www.linkedin.com/in/abdul-hannan-8a78b01bb/"> <i style="color: white;" class="fa-brands fa-linkedin-in"></i> </a>
+            <a target="_blank" href="mailto:abdulhannan8540680@gmail.com"> <i style="color: white;" class="fa-regular fa-envelope"></i> </a>
+        </div>
+        </div>
+
+    </div>
+
+    <?php include("footer.html") ?>
+
+</body>
+
+</html>
