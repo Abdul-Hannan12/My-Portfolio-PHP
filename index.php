1b82a3f0eb2653ccb6129012753c9a99233093af
Abdul Hannan <abdulhannan8540680@gmail.com>
2022-09-20 23:09:53 +0500

    added button on landing page

--- index.php
+++ index.php
@@ -9,39 +9,49 @@
 
     <title>Portfolio</title>
 
-    <link rel="stylesheet" href="styles/index.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="styles/index.css">
 
     <script src="https://kit.fontawesome.com/0980d95b57.js" crossorigin="anonymous"></script>
 
 </head>
 
 <body>
-
-    <?php include("header.html") ?>
     
-    <div class="cover">
+    <div class="container">
+
+        <?php include("header.html") ?>
         
-        <div class="heading">
-        <h3 class="name_heading">Abdul Hannan</h3>
-        <p class="name_sub_heading">WEB AND APP DEVELOPER</p>
+        <div class="cover">
+            
+            <div class="heading">
+            <h3 class="name_heading">Abdul Hannan</h3>
+            <p class="name_sub_heading">WEB AND APP DEVELOPER</p>
+            </div>
+
+            <div class="heading_para">
+            <p class="head_para"> I am a passionate developer, full of motivation to move forward. I have a passion for learning new things and the curiosity is what keeps me going on the journey of development. </p>
+            <div class="icons">
+                <a target="_blank" href="mailto:abdulhannan8540680@gmail.com"> <i style="color: white;" class="fa-regular fa-envelope"></i> </a>
+                <a target="_blank" href="https://github.com/Rocky-Khan"> <i style="color: white;" class="fa-brands fa-github"></i> </a>
+                <a target="_blank" href="https://www.linkedin.com/in/abdul-hannan-8a78b01bb/"> <i style="color: white;" class="fa-brands fa-linkedin-in"></i> </a>
+            </div>
+            </div>
+
+            <div class="image">
+                <img class="my_image" src="assets/images/me.png" alt="image"/>
+            </div>
+
         </div>
 
-        <div class="heading_para">
-        <p class="head_para">Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ex eum quia, fugiat delectus error adipisci vitae ab ducimus excepturi eligendi ad aliquid recusandae?</p>
-        <div class="icons">
-            <a target="_blank" href="mailto:abdulhannan8540680@gmail.com"> <i style="color: white;" class="fa-regular fa-envelope"></i> </a>
-            <a target="_blank" href="https://github.com/Rocky-Khan"> <i style="color: white;" class="fa-brands fa-github"></i> </a>
-            <a target="_blank" href="https://www.linkedin.com/in/abdul-hannan-8a78b01bb/"> <i style="color: white;" class="fa-brands fa-linkedin-in"></i> </a>
-        </div>
+        <div class="bottom">
+            <a href="contact.php" class="btn">
+                <img src="assets/images/btn.png" alt="button_works">
+            </a>
         </div>
 
-        <div class="image">
-            <img class="my_image" src="assets/images/me.png" alt="image"/>
-        </div>
+        <?php include("footer.html") ?>
 
     </div>
-
-    <?php include("footer.html") ?>
 
 </body>
 
